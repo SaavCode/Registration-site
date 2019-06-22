f0ff114595860dbafa96cf1fcd0265c1efc55d9b
Nestor Saavedra <saavfoxdev@gmail.com>
2019-06-22 10:00:33 -0700

    script

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.post("/register", function(req, res){
     var person = {
         email: req.body.email
     };
+
     connection.query('INSERT INTO users SET ?', person, function(err, result) {
         if (err) throw err;
         res.redirect("/");
